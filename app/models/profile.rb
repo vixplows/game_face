7c12f2da568d5468c2212e6e1430bdc98a8de8a3
vixplows <vicky@vickyplows.co.uk>
2017-09-05 15:21:30 +0100

    Install bootstrap gem

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,7 @@
 class Profile < ActiveRecord::Base
+
+  validates :username, presence: true, length: {minimum: 6, maximum: 20}
+
+  validates :bio, presence: true, length: {minimum: 10, maximum: 150}
+
 end
